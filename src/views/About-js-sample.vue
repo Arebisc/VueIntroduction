bc8f4178dafb516a873c89389c8a34925511bf5b
Michał Sibera <michal.sibera@flexdevgroup.com>
2020-11-16 13:05:26 +0100

    Added About-js-sample.vue

---
+++ src/views/About-js-sample.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <h2>{{ reversedMessage }}</h2>
+        <button @click="changeMessage">Change</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            sampleMessage: 'message',
+        };
+    },
+    computed: {
+        reversedMessage() {
+            return this.sampleMessage
+                .split('')
+                .reverse()
+                .join('');
+        },
+    },
+    methods: {
+        changeMessage() {
+            this.sampleMessage = 'other';
+        },
+    },
+};
+</script>
