40aeaaabaf027dae0ba56662e0d9a21352d9af63
Michał Sibera <michal.sibera@flexdevgroup.com>
2020-11-16 10:51:38 +0100

    Sample ts class syntax in About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <h2>{{ reversedMessage }}</h2>
+        <button @click="changeMessage">Change</button>
     </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+    private sampleMessage = 'message';
+
+    private get reversedMessage() {
+        return this.sampleMessage
+            .split('')
+            .reverse()
+            .join('');
+    }
+
+    private changeMessage() {
+        this.sampleMessage = 'other';
+    }
+}
+</script>
