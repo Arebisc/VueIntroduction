bd0cb0681922f68e68f78f5373731a991f1ff1dc
Michal Sibera <michal.sibera@windowslive.com>
2020-11-17 13:09:39 +0100

    Added computed vs methods sample

---
+++ src/views/ComputedSample.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Computed properties vs methods reactivity sample</h1>
+
+        <button @click="a++">Increment A</button>
+        <button @click="b++">Increment B</button>
+
+        <h2>A: {{ a }}</h2>
+        <h2>B: {{ b }}</h2>
+
+        <h2>A + Age: {{ addToA() }}</h2>
+        <h2>B + Age: {{ addToB() }}</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class ComputedSample extends Vue {
+    private age = 25;
+    private a = 0;
+    private b = 0;
+
+    private addToA() {
+        console.log('AddToA method');
+        return this.a + this.age;
+    }
+
+    private addToB() {
+        console.log('AddToB method');
+        return this.b + this.age;
+    }
+
+    private get computedA() {
+        console.log('Computed A');
+        return this.a + this.age;
+    }
+
+    private get computedB() {
+        console.log('Computed B');
+        return this.b + this.age;
+    }
+}
+</script>
